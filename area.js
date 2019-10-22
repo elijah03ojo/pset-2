979272fff6e405ed4e0cf771e20922e3098fc387
elijah03ojo <44117114+elijah03ojo@users.noreply.github.com>
2019-10-22 13:04:49 -0400

    Update area.js

--- area.js
+++ area.js
@@ -1,1 +1,8 @@
+const readlineSync = require("readline-sync")
+
+let width = readlineSync.question("\n What is the width? \n");
+let length = readlineSync.question("\n What is the length? \n");
+
+
+console.log("\nA " + width +  " by " + length + " inch sheet of paper has an area of " + width * length + " inches")
 
