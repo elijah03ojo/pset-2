8009c59afceb2ba2bf83d7c2c4b178f53547e14c
elijah03ojo <44117114+elijah03ojo@users.noreply.github.com>
2019-10-24 12:03:09 -0400

    Update diagonal.js

--- diagonal.js
+++ diagonal.js
@@ -1,1 +1,13 @@
-Not done yet 
+const readlineSync = require("readline-sync");
+
+const width = readlineSync.question("\nWidth: ");
+const length = readlineSync.question("Length: ");
+const diagonal = c.toLocaleString("en", {minimumFractionDigits: 2, maximumFractionDigits: 2});
+
+let a = width ;
+let b = length ;
+let c = Math.hypot(a,b) ;
+
+
+
+console.log("\nA(n) " + width + "-by-" + length + "-inch sheet of paper has a diagonal of " + diagonal + " inch(es).");
