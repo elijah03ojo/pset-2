3777f8b4373bd91c3806b33883ffd08f08916560
elijah03ojo <44117114+elijah03ojo@users.noreply.github.com>
2019-10-24 11:45:13 -0400

    Update perimeter.js

--- perimeter.js
+++ perimeter.js
@@ -4,4 +4,4 @@ let width = readlineSync.question("\n width: \n");
 let length = readlineSync.question("\n length: \n");
 
 
-console.log("\nA(n) " + width +  " by " + length + " inch sheet of paper has an area of " + (width * length) * 25.4 ** 2 + " square millimeter(s)")
+console.log("\nA(n) " + width +  "-by-" + length + " inch sheet of paper has a perimeter of " + (2*width + 2*length) * (2.54)  + " centimeters(s)")
