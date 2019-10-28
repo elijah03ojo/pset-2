3ab4d52d3e2f2f004391f25a6e63d521ad0dc90f
elijah03ojo <44117114+elijah03ojo@users.noreply.github.com>
2019-10-28 11:45:55 -0400

    Add files via upload

---
+++ hourly.js
@@ -0,0 +1,17 @@
+
+const readlineSync = require("readline-sync");
+
+const hourlyWage = Number(readlineSync.question("\nHourly wage: "));
+const monHour = Number(readlineSync.question("\nMonday: "));
+const tueHour = Number(readlineSync.question("Tuesday: "));
+const wedHour = Number(readlineSync.question("Wednesday: "));
+const thuHour = Number(readlineSync.question("Thursday: "));
+const friHour = Number(readlineSync.question("Friday: "));
+const satHour = Number(readlineSync.question("Saturday: "));
+const sunHour = Number(readlineSync.question("Sunday: "));
+
+const wage = 12.5 ;
+const earnings = (monHour * wage) + (tueHour * wage) + (wedHour * wage) + (thuHour * wage) + (friHour * wage) + (satHour * wage) + (sunHour * wage)
+const earnf = earnings.toLocaleString("en", {minimumFractionDigits: 2, maximumFractionDigits: 2});
+
+console.log("\nYou'll make $" + earnf + " this week.");
